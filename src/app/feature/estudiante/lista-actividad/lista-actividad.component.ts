f16a356520abd509f4e264d1f21c46a9b116cae2
Jhonny Esneider Guarin Chavez <jhonnyesneidergcha@ufps.edu.co>
2023-06-03 17:45:40 -0500

    estilo y contenido lista de actividades

--- src/app/feature/estudiante/lista-actividad/lista-actividad.component.ts
+++ src/app/feature/estudiante/lista-actividad/lista-actividad.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { ActividadService } from 'src/app/core/services/actividad.service';
+import { TemaService } from 'src/app/core/services/tema.service';
 
 @Component({
   selector: 'app-lista-actividad',
@@ -7,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class ListaActividadComponent {
 
+  actividades: any = []
+
+  constructor(private actividadService: ActividadService, private sanitizer: DomSanitizer) 
+  {   
+    this.actividadService.listar().subscribe(data => {
+      this.actividades = data
+    })
+  }
+  sanitizeUrl(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
